05c9e4c00f1b63eae5cbb0c81b81a0da941c0944
Dominika Fudala <fud.dominika@gmail.com>
2021-06-08 17:32:53 +0200

    GUI + working game

--- interface.py
+++ interface.py
@@ -1,113 +1,179 @@
-# from tkinter import *
-# from tkinter import ttk
-
-# # Tworzenie okna
-# window = Tk()
-# window.title("Skarbonka")
-# # Tworzenie siatki na przyciski
-# mainframe = ttk.Frame(window)
-# # Umieszczenie siatki w oknie
-# window.columnconfigure([0, 1, 2], minsize=100)
-# window.rowconfigure([0, 1], minsize=100)
-# # # Dodanie przycisków do wrzucania monet
-# # i=0
-# # for c in COINS:
-# #     ttk.Button(mainframe, text="Wrzuć "+str(c)+"zł", command=lambda c=c: pb.add(Coin(c))).grid(column=2, row=i)
-# #     i+=1
-# # # Dodanie przycisku sprawdzenia wartości zawartości
-# ttk.Button(text="Przerwij", command=lambda: print("tekst")).grid(column=0, row=0)
-
-# window.mainloop()
-
+import mastermind
 from tkinter import *
 from tkinter import ttk
 
-# root
+
+# self.__root
 class Interface:
     def __init__(self):
-        root = Tk()
-        root.title("Mastermind")
-        root.geometry("{}x{}".format(1200, 800))
+        self.__odpowiedzi_usun = []
+        self.__game = mastermind.gameMastermind()
+        self.__root = Tk()
+        self.__root.title("Mastermind")
+        self.__root.geometry("{}x{}".format(1200, 800))
 
         # two main columns
-        left_side = ttk.Frame(root)
-        right_side = ttk.Frame(root)
+        self.__left_side = ttk.Frame(self.__root)
+        self.__right_side = ttk.Frame(self.__root)
 
-        root.rowconfigure(0, weight=1)
-        root.columnconfigure([0, 1], weight=1)
+        self.__left_top_frame = ttk.Frame(self.__left_side)
+        self.__left_bottom_frame = ttk.Frame(self.__left_side)
 
-        left_side.grid(row=0, column=0)
-        right_side.grid(row=0, column=1)
+    def interfaceDraw(self):
+        self.__root.rowconfigure(0, weight=1)
+        self.__root.columnconfigure([0, 1], weight=1)
+
+        self.__left_side.grid(row=0, column=0)
+        self.__right_side.grid(row=0, column=1)
 
         # left side wit entry and buttons
-        left_side.rowconfigure([0, 1], weight=1)
-        left_side.columnconfigure(0, weight=1)
+        self.__left_side.rowconfigure([0, 1], weight=1)
+        self.__left_side.columnconfigure(0, weight=1)
 
-        left_top_frame = ttk.Frame(left_side)
-        left_bottom_frame = ttk.Frame(left_side)
-
-        left_top_frame.grid(row=0, column=0)
-        left_bottom_frame.grid(row=1, column=0)
+        self.__left_top_frame.grid(row=0, column=0)
+        self.__left_bottom_frame.grid(row=1, column=0)
 
         # entry
-        left_top_frame.rowconfigure([0, 1], weight=1)
-        left_top_frame.columnconfigure(0, weight=1)
+        self.__left_top_frame.rowconfigure([0, 1], weight=1)
+        self.__left_top_frame.columnconfigure(0, weight=1)
         entry_var = StringVar()
         entry_var.set("")
-        text = ttk.Label(
-            left_top_frame,
-            text="Pozostało " + "x" + " prób",
+        Entry(
+            self.__left_top_frame,
+            textvariable=entry_var,
             font=("calibre", 20, "normal"),
-        ).grid(row=0, column=0)
-        entry = Entry(
-            left_top_frame, textvariable=entry_var, font=("calibre", 20, "normal")
         ).grid(row=1, column=0)
 
-        left_bottom_frame.rowconfigure(0, weight=1, minsize=70)
-        left_bottom_frame.columnconfigure([0, 1, 2], weight=1)
+        self.__left_bottom_frame.rowconfigure(0, weight=1, minsize=70)
+        self.__left_bottom_frame.columnconfigure([0, 1, 2], weight=1)
+
+        self.__proby = ttk.Label(
+            self.__left_top_frame,
+            text="",
+            font=("calibre", 20, "normal"),
+        )
+        self.__proby.grid(row=0, column=0)
+
+        # przyciski
         ttk.Button(
-            left_bottom_frame, text="Sprawdź", command=lambda: print(entry_var.get())
+            self.__left_bottom_frame,
+            text="Sprawdź",
+            command=lambda: self.writeAnswers(
+                self.__game.userInputGuess(entry_var.get())
+            ),
         ).grid(column=0, row=0)
         ttk.Button(
-            left_bottom_frame, text="Oszust", command=lambda: print(entry_var.get())
+            self.__left_bottom_frame,
+            text="Oszust",
+            command=lambda: self.oszust(),
         ).grid(column=1, row=0)
         ttk.Button(
-            left_bottom_frame, text="Reset", command=lambda: print(entry_var.get())
+            self.__left_bottom_frame,
+            text="Reset",
+            command=lambda: self.resetGame(),
         ).grid(column=2, row=0)
 
         # answers column
-        right_side.rowconfigure([x for x in range(13)], weight=1, minsize=46)
-        right_side.columnconfigure([x for x in range(3)], weight=1, minsize=100)
+        self.__right_side.rowconfigure([x for x in range(13)], weight=1, minsize=46)
+        self.__right_side.columnconfigure([x for x in range(3)], weight=1, minsize=100)
         ttk.Label(
-            right_side,
+            self.__right_side,
             text="Właściwe |",
             font=("calibre", 18, "normal"),
         ).grid(row=0, column=0)
         ttk.Label(
-            right_side,
+            self.__right_side,
             text=" Odpowiedzi ",
             font=("calibre", 18, "normal"),
         ).grid(row=0, column=1)
         ttk.Label(
-            right_side,
+            self.__right_side,
             text="| Niewłaściwe",
             font=("calibre", 18, "normal"),
         ).grid(row=0, column=2)
-        for i in range(12):
-            ttk.Label(
-                right_side,
-                text="tu wlasciwe",
-                font=("calibre", 10, "normal"),
-            ).grid(row=i + 1, column=0)
-            ttk.Label(
-                right_side,
-                text=str(i + 1) + " " + "kod",
-                font=("calibre", 10, "normal"),
-            ).grid(row=i + 1, column=1)
-            ttk.Label(
-                right_side,
-                text="tu niewlasciwe",
-                font=("calibre", 10, "normal"),
-            ).grid(row=i + 1, column=2)
 
-        root.mainloop()
+        self.__root.mainloop()
+
+    def writeAnswers(self, list):
+        if list == -1:
+            self.__proby.grid_remove()
+            self.__proby = ttk.Label(
+                self.__left_top_frame,
+                text="Niepoprawny kod",
+                font=("calibre", 20, "normal"),
+            )
+            self.__proby.grid(row=0, column=0)
+            return
+        if list != None:
+            if list == "wygrana":
+                self.__proby.grid_remove()
+                self.__proby = ttk.Label(
+                    self.__left_top_frame,
+                    text="Wygrana(Reset jesli chcesz zagrac ponownie)",
+                    font=("calibre", 20, "normal"),
+                )
+                self.__proby.grid(row=0, column=0)
+                return
+
+            self.__proby.grid_remove()
+            self.__proby = ttk.Label(
+                self.__left_top_frame,
+                text="Pozostało " + str(list[2]) + " prób",
+                font=("calibre", 20, "normal"),
+            )
+            self.__proby.grid(row=0, column=0)
+            i = 12 - list[2]
+            wlasciwe = ttk.Label(
+                self.__right_side,
+                text=str(list[0]),
+                font=("calibre", 10, "normal"),
+            )
+            wlasciwe.grid(row=i, column=0)
+            kod = ttk.Label(
+                self.__right_side,
+                text=str(self.__game.returnUserGuesses()[i - 1]),
+                font=("calibre", 10, "normal"),
+            )
+            kod.grid(row=i, column=1)
+            niewlasciwe = ttk.Label(
+                self.__right_side,
+                text=str(list[1]),
+                font=("calibre", 10, "normal"),
+            )
+            niewlasciwe.grid(row=i, column=2)
+
+            self.__odpowiedzi_usun.append([wlasciwe, kod, niewlasciwe])
+            if list[2] == 0:
+                self.__proby.grid_remove()
+                self.__proby = ttk.Label(
+                    self.__left_top_frame,
+                    text="Przegrana(Reset jesli chcesz zagrac ponownie)",
+                    font=("calibre", 20, "normal"),
+                )
+                self.__proby.grid(row=0, column=0)
+                return
+
+    def resetGame(self):
+        self.__game = mastermind.gameMastermind()
+        self.__proby.grid_remove()
+        for tabela in self.__odpowiedzi_usun:
+            for czesc in tabela:
+                czesc.grid_remove()
+
+    def oszust(self):
+        if isinstance(self.__game, mastermind.mastermind):
+            self.__proby.grid_remove()
+            self.__proby = ttk.Label(
+                self.__left_top_frame,
+                text="Tere fere",
+                font=("calibre", 20, "normal"),
+            )
+            self.__proby.grid(row=0, column=0)
+        else:
+            self.__proby.grid_remove()
+            self.__proby = ttk.Label(
+                self.__left_top_frame,
+                text="Złapałeś mnie!(Reset jesli chcesz zagrac ponownie)",
+                font=("calibre", 20, "normal"),
+            )
+            self.__proby.grid(row=0, column=0)
---
+++ mastermind.py
@@ -0,0 +1,141 @@
+import random
+import re
+from exceptions import IncorrectInput
+
+from abc import ABC, abstractmethod
+
+
+class RegulyGry(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def guess(self, userGuess):
+        pass
+
+    @abstractmethod
+    def userInputGuess(self, entry):
+        pass
+
+    @abstractmethod
+    def returnUserGuesses(self):
+        pass
+
+
+class mastermind(RegulyGry):
+    def __init__(self):
+        self.__attempts = 12
+        self.__solution = [random.randrange(1, 7, 1) for i in range(1, 5)]
+        self.__allUserGuesses = []
+        self.__listInfo = []
+        print(self.__solution)
+
+    def guess(self, userGuess):
+        if self.__attempts > 0:
+            self.__listInfo = []
+            rightSpot = 0
+            rightNum = 0
+            checkedNum = []
+
+            if userGuess == self.__solution:
+                return "wygrana"
+
+            for i in range(0, len(userGuess)):
+                if userGuess[i] not in checkedNum:
+                    checkedNum.append(userGuess[i])
+                    if userGuess[i] in self.__solution:
+                        numOfSameElementsUser = userGuess.count(
+                            userGuess[i]
+                        )  # ile takich samych liczb w zgadywanym
+                        numOfSameElements = self.__solution.count(
+                            userGuess[i]
+                        )  # ile takich samych liczb w rozwiazaniu
+                        checkSameElements = numOfSameElements - numOfSameElementsUser
+                        if checkSameElements == 0:
+                            rightNum += numOfSameElements  # jesli liczba zgadnietych liczb jest rowna liczb w rozwiazaniu
+                        elif checkSameElements > 0:
+                            rightNum += numOfSameElementsUser  # jesli wiecej liczb jest w rozwiazaniu dodajemy tylko liczbe w zgadnietym
+                        else:
+                            rightNum += numOfSameElements  # jesli wiecej liczb jest w zgadnietym dodajemy tylko liczbe w rozwiazaniu
+                if userGuess[i] == self.__solution[i]:
+                    rightSpot += 1  # jesli liczba jest na swoim miejscu
+                    rightNum -= 1  # odjęcie od na niewłaściwym miejscu
+
+            self.__attempts -= 1
+            self.__listInfo.append(rightSpot)
+            self.__listInfo.append(rightNum)
+            self.__listInfo.append(self.__attempts)
+
+            return self.__listInfo.copy()
+
+    def userInputGuess(self, entry):
+        try:
+            num_format = re.compile(r"^([1-6][1-6][1-6][1-6])$")
+            if not re.match(num_format, entry):
+                raise IncorrectInput("Niepoprawny kod")
+        except IncorrectInput:
+            return -1
+
+        userGuess = [int(num) for num in entry]
+        self.__allUserGuesses.append(userGuess)
+
+        return self.guess(userGuess)
+
+    def returnUserGuesses(self):
+        return self.__allUserGuesses.copy()
+
+
+# w wersji oszusta nawet nie generujemy popranwej odpowiedzi jako ze ta odpowiedz nie bedzie brana pod uwage
+class oszustMastermind(RegulyGry):
+    def __init__(self):
+        self.__attempts = 12
+        self.__listInfo = []
+        self.__allUserGuesses = []
+
+    def guess(self, userGuess):
+        if self.__attempts > 0:
+            self.__listInfo = []
+
+            rightSpot = 0
+            rightNum = 0
+
+            rightSpot = random.randrange(
+                0, 5, 1
+            )  # przy oszuscie wybieramy loswą liczbe na wlasciwym miejscu
+            rightNum = random.randrange(
+                0, 5 - rightSpot, 1
+            )  # wybieramy losową liczbe ale taka zeby suma rightSpot i rightNum nie była większa od 4
+
+            self.__attempts -= 1
+            self.__listInfo.append(rightSpot)
+            self.__listInfo.append(rightNum)
+            self.__listInfo.append(self.__attempts)
+
+            return self.__listInfo.copy()
+
+    def userInputGuess(self, entry):
+        try:
+            num_format = re.compile(r"^([1-6][1-6][1-6][1-6])$")
+            if not re.match(num_format, entry):
+                raise IncorrectInput("Niepoprawny kod")
+        except IncorrectInput:
+            return -1
+
+        userGuess = [int(num) for num in entry]
+        self.__allUserGuesses.append(userGuess)
+
+        return self.guess(userGuess)
+
+    def returnUserGuesses(self):
+        return self.__allUserGuesses.copy()
+
+
+def gameMastermind():
+    randomgame = random.randrange(
+        2
+    )  # losujemy czy zagramy normalnego masterminda czy oszukiwanego
+    if randomgame == 0:
+        return mastermind()
+    else:
+        return oszustMastermind()
